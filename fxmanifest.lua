a032e327fd641709296da41a3f300b590c6120a1
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2025-06-08 23:13:18 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,13 +2,9 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'FalseHope'
-description 'Strawberry Gardens Design by VerpiModDz Updated and Reworked by FalseHopeDesigns'
+
+description 'Strawberry Gardens Legion Square by VerpiModDz Updated and Reworked by FalseHopeDesigns'
+
 version '1.0.0'
 
 this_is_a_map 'yes'
-
--- Escrow ignore for all files
-escrow_ignore {
-    'stream/*'
-    'data/*'
-}
