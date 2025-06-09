67c1c8febd1deb206ed9688848c4d1ec56833b4c
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2025-06-08 22:37:39 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'FalseHope'
+description 'Strawberry Gardens Design by VerpiModDz Updated and Reworked by FalseHopeDesigns'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+-- Escrow ignore for all files
+escrow_ignore {
+    'stream/*'
+    'data/*'
+}
